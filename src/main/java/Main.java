a1a1c04075975849b29f8dc29c621984873f518b
jalfonsom1506 <jalfonsom1506@gmail.com>
2026-07-23 16:31:10 +0200

    Estructura de la clase Main con la composición del array de productos y simulación del flujo de información en Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,40 @@ public class Main {
 
     static void main(String[] args) {
 
-        Comida callos = new Comida ("Callos", 9.75, "Comida", true);
-        callos.detalles();
+        Cliente ana = new Cliente ("Ana López", "600123456");
+        Camarero carlos = new Camarero("Carlos", "010");
+
+        Comida ensaladilla = new Comida ("Ensaladilla rusa", 9.70, "Raciones", false);
+        Comida polloAsado = new Comida ("Pollo asado", 12.80, "Platos Principales", true);
+        Comida croquetasJamon = new Comida ("Croquetas de jamón", 10.80, "Raciones", true);
+        Comida sandwichVegetal = new Comida ("Sandwich vegetal", 7.50, "Bocadillos", false);
+
+        Bebida aguaGas = new Bebida ("Agua con gas", 3.20, "Aguas", "Mediano");
+        Bebida zumoNaranja = new Bebida ("Zumo de naranja natural", 4.30, "Zumos", "Pequeño");
+        Bebida cocaCola = new Bebida ("Coca Cola", 2.50, "Refrescos", "330cl");
+        Bebida cafe = new Bebida ("Café con leche", 1.80, "Cafés", "Pequeño");
+
+        Ticket ticket = new Ticket (ana, carlos);
+
+        ticket.agregarProducto(aguaGas);
+        ticket.agregarProducto(cocaCola);
+        ticket.agregarProducto(sandwichVegetal);
+        ticket.agregarProducto(ensaladilla);
+
+        ticket.mostrarTicket();
+
+        double porcentaje = 20.0;
+        double promoAguaGas = aguaGas.aplicarDescuento(porcentaje);
+        aguaGas.setPrecio(promoAguaGas);
+
+        System.out.println("\n------- Promoción Aplicada -------");
+        System.out.printf("%s: %.2f%%\n", aguaGas.getNombre(), porcentaje);
+        System.out.printf("Precio final: %.2f€\n", promoAguaGas);
+        System.out.printf("\nTOTAL CON DESCUENTO: %.2f€\n", ticket.calcularTotal());
+        System.out.println("----------------------------------\n");
+
+        System.out.println("\n***** Muchas gracias por su visita *****");
+
+
     }
 }
--- src/main/java/Ticket.java
+++ src/main/java/Ticket.java
@@ -4,4 +4,47 @@ public class Ticket {
     private Camarero camarero;
     private Producto[] productos;
     private int contadorProductos;
+
+    public Ticket(Cliente cliente, Camarero camarero) {
+        this.cliente = cliente;
+        this.camarero = camarero;
+        this.productos = new Producto[10];
+        this.contadorProductos = 0;
+    }
+
+    public void agregarProducto(Producto nuevoProducto) {
+        if (this.contadorProductos < this.productos.length) {
+            this.productos[this.contadorProductos] = nuevoProducto;
+            this.contadorProductos++;
+        } else {
+            System.out.println("No se pueden agregar más productos a este ticket");
+        }
+    }
+
+    public double calcularTotal() {
+        double total = 0.0;
+
+        for (int i = 0; i < this.contadorProductos; i++) {
+            total = total + this.productos[i].getPrecio();
+        }
+        return total;
+    }
+
+    public void mostrarTicket() {
+        System.out.println("\n==========================");
+        System.out.println("=     CAFETERIA JAVA     =");
+        System.out.println("==========================\n");
+
+        this.cliente.detallesCliente();
+        this.camarero.detallesCamarero();
+
+        System.out.println("\n        PRODUCTOS");
+
+        for(int i = 0; i < this.contadorProductos; i++){
+            System.out.printf("%d. %s: %.2f€\n", (i + 1), this.productos[i].getNombre()
+            ,this.productos[i].getPrecio());
+        }
+
+        System.out.printf("\nTOTAL: %.2f€\n", this.calcularTotal());
+    }
 }
